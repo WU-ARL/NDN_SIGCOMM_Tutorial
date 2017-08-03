22432ae333835e0644014c02b690402293776d4b
John DeHart <jdd@johns-mbp-2.seas.wustl.edu>
2017-08-03 17:30:50 -0500

    force a client prefix

--- Ansible/roles/producer-gw/files/nfdcCommand.sh
+++ Ansible/roles/producer-gw/files/nfdcCommand.sh
@@ -2,6 +2,7 @@
 
 PRODUCER=`cat producer_hostname`
 
-nfdc route add /ndn/edu/wustl/jdd udp4://${PRODUCER}
+#nfdc route add /ndn/edu/wustl/jdd udp4://${PRODUCER}
+nfdc register -o 65 /ndn/edu/wustl/jdd udp4://${PRODUCER}
 
 
