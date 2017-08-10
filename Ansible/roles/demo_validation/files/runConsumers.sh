83ade83b31329d8f5bab367f5245e8749a8622f9
John DeHart <jdd@johns-mbp-2.seas.wustl.edu>
2017-08-10 17:09:37 -0500

    more validation stuff

---
+++ Ansible/roles/demo_validation/files/runConsumers.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+SUFFIX="000"
+if [ $# -eq 1 ]
+then
+  SUFFIX=$1
+fi
+
+cd /home/ubuntu/validation
+
+# make sure we don't have another already running:
+killall ndncatchunks
+
+# ndncatchunks command goes here:
+nohup ndncatchunks -c /home/ubuntu/validation/consumer2.cfg /ndn/edu/wustl/jdd/file_01_01.${SUFFIX} > file_01_01.png &
+sleep 2
+nohup ndncatchunks -c /home/ubuntu/validation/consumer2.cfg /ndn/edu/wustl/jdd/file_01_02.${SUFFIX} > file_01_02.png &
+sleep 2
+nohup ndncatchunks -c /home/ubuntu/validation/consumer2.cfg /ndn/edu/wustl/jdd/file_02_01.${SUFFIX} > file_02_01.png &
+sleep 2
+nohup ndncatchunks -c /home/ubuntu/validation/consumer2.cfg /ndn/edu/wustl/jdd/file_02_02.${SUFFIX} > file_02_02.png &
+sleep 2
+
---
+++ Ansible/runValidationDemo.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+SUFFIX="000"
+if [ $# -eq 1 ]
+then
+  SUFFIX=$1
+fi
+
+ansible-playbook -u ubuntu --ssh-extra-args="-i ~/.ssh/aws_wu_jdd.pem -o \"StrictHostKeyChecking no\"" --ask-vault-pass -i inventory --extra-vars "suffix=${SUFFIX}" runDemoValidation.yml
+
+pushd ../Laptop
+./runAllConsumers.sh ${SUFFIX}
+
+sleep 5
+
+pushd ./image_slicer
+nohup ./join_show_image_consumer1.sh ../CONSUMER1 &
+nohup ./join_show_image_consumer2.sh ../CONSUMER2 &
+nohup ./join_show_image_consumer3.sh ../CONSUMER3 &
+
+sleep 5
+
+popd
+popd
+
